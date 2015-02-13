a792117d7abb23649c1243b3a8a2ddeeb9ad4e21
gilead <janne.rasanen@iki.fi>
2015-02-13 19:47:41 +0200

    Tires now turn and follow suspension better

--- Assets/Scripts/WheelSync.cs
+++ Assets/Scripts/WheelSync.cs
@@ -5,13 +5,31 @@ public class WheelSync : MonoBehaviour {
     public WheelCollider collider;
 
     private Vector3 tmp, basePos;
+    private WheelHit wheelHit = new WheelHit();
 
     void Start() {
         basePos = transform.localPosition;
     }
 
 	void Update () {
-        transform.localPosition = basePos + collider.center;
-        transform.Rotate(-collider.rpm * 6 * Time.deltaTime, 0, 0);
+        // spin the wheel to create illusion of moving
+        transform.Rotate(collider.rpm * 6 * Time.deltaTime, 0, 0);
+
+        // turn the wheel to match steering
+        tmp = transform.localEulerAngles;
+        tmp.y = collider.steerAngle - transform.localEulerAngles.z;
+        transform.localEulerAngles = tmp;
+        
+
+        // move the wheel to match the suspension
+        if (collider.GetGroundHit(out wheelHit))
+        {
+            transform.position = wheelHit.point + collider.transform.up * collider.radius;
+        }
+        else
+        {
+            // the wheel is in air
+            transform.position = collider.transform.position - collider.transform.up * collider.suspensionDistance;
+        }
 	}
 }
